dea427fd272ac38d687dd9f794f57167795c0060
Hydroxya22 <mjgarcia0110j@gmail.com>
2023-10-21 23:04:29 -0500

    Designed leaderboard card

--- modules/AeroFarallonesTheme/Resources/views/widgets/leader_board.blade.php
+++ modules/AeroFarallonesTheme/Resources/views/widgets/leader_board.blade.php
@@ -1,7 +1,33 @@
 {{-- Incio de las tarjetas leader --}}
 
 {{-- @if ($title) --}}
-<div class="w-50 h-100 bg-warning">
-
+<div class="w-50 h-100 bg-warning border border-black"  >
+    <div class="border border-black">
+        <span>Leaderboard | October (2023)</span>
+        
+    </div>
+    <div>
+        <div class="container text-center">
+            <div class="row">
+              <div class="col">
+                Pilot
+                <ul style="list-style-type: none">
+                    <li>Marlon</li>
+                    <li>Martin</li>
+                    <li>Julian</li>
+                </ul>
+              </div>
+              <div class="col">
+                Record
+                <ul style="list-style-type: none">
+                    <li>44</li>
+                    <li>44</li>
+                    <li>44</li>
+                </ul>
+              </div>
+        <div>
+            <span>Flights</span>
+        </div>
+    </div>
 </div>
 {{-- @endif --}}

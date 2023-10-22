2213e1eb202e3359ad600d402de89493eff4f3b7
Hydroxya22 <mjgarcia0110j@gmail.com>
2023-10-21 23:30:25 -0500

    Designed the 80% of the leaderboard card

--- modules/AeroFarallonesTheme/Resources/views/widgets/leader_board.blade.php
+++ modules/AeroFarallonesTheme/Resources/views/widgets/leader_board.blade.php
@@ -1,14 +1,14 @@
 {{-- Incio de las tarjetas leader --}}
 
 {{-- @if ($title) --}}
-<div class="w-50 h-5 bg-warning border border-black margin mt-4"  >
-    <div class="border border-black">
+<div class="w-25 border rounded border-black margin mt-4" style="height: 12rem">
+    <div class="border border-black text-white" style="background-color: #00157f; font-weight:bold">
         <span>Leaderboard | October (2023)</span>
     </div>
     <div>
         <div class="container text-center">
             <div class="row">
-              <div class="col">
+              <div class="col text-start">
                 Pilot
                 <ul style="list-style-type: none">
                     <li>Marlon</li>
@@ -16,7 +16,7 @@
                     <li>Julian</li>
                 </ul>
               </div>
-              <div class="col">
+              <div class="col text-end">
                 Record
                 <ul style="list-style-type: none">
                     <li>44</li>

f671d231a220018557d390dc7ecb7d4b5b8e27a9
Hydroxya22 <mjgarcia0110j@gmail.com>
2023-11-21 23:21:37 -0500

    Leaderboard card initial set up and styling

--- modules/FlsModule/Resources/views/widgets/leader_board.blade.php
+++ modules/FlsModule/Resources/views/widgets/leader_board.blade.php
@@ -1,20 +1,54 @@
 {{-- @if(filled($leader_board)) --}}
 <div class="card mb-2">
-  <div class="card-header p-1">
-    <h5 class="m-1">
+  <div class="card-header p-1" style="background-color: #000a54">
+    <h5 class="m-1 text-white">
       {{ $header_title }}
       <i class="fas {{ $header_icon }} float-end"></i>
     </h5>
   </div>
   <div class="card-body p-0 table-responsive">
-    <table class="table table-sm table-striped table-borderless text-start text-nowrap align-middle mb-0">
+    <table class="table table-sm table-borderless text-start text-nowrap align-middle mb-0">
       @if($count > 1)
       <tr>
-        <th>@lang('FlsModule::common.name')</th>
+        <th>@lang('FlsModule::common.pilots')</th>
         <th class="text-end">{{ $column_title }}</th>
       </tr>
       @endif
-      @foreach($leader_board as $board)
+      <tr>
+        <td>
+          <a href="">
+            Pepito
+          </a>
+        </td>
+        <td class="text-end">5</td>
+      </tr>
+      <tr>
+        <td>
+          <a href="">
+            Pepito
+          </a>
+        </td>
+        <td class="text-end">5</td>
+      </tr>
+      <tr>
+        <td>
+          <a href="">
+            Pepito
+          </a>
+        </td>
+        <td class="text-end">5</td>
+      </tr>
+    </table>
+  </div>
+  <div class="card-footer p-0 px-1 text-center small fw-bold border rounded-pill" style="border-color: #00157f">
+    {{ $footer_note.' '.$footer_type }}
+  </div>
+</div>
+{{-- @endif --}}
+
+
+
+      {{-- @foreach($leader_board as $board)
       <tr>
         <td>
           <a href="{{ route($board['route'], $board['id']) }}">
@@ -26,11 +60,4 @@
         </td>
         <td class="text-end">{{ $board['totals'] }}</td>
       </tr>
-      @endforeach
-    </table>
-  </div>
-  <div class="card-footer p-0 px-1 text-end small fw-bold">
-    {{ $footer_note.' '.$footer_type }}
-  </div>
-</div>
-{{-- @endif --}}
+      @endforeach --}}

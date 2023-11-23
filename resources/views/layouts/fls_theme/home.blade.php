e2aa5d199852ea51e42e78f2ca2d9d9515e09d90
rcomunica <julir2772@gmail.com>
2023-11-23 15:08:51 -0500

    new image (Landing page)

--- resources/views/layouts/fls_theme/home.blade.php
+++ resources/views/layouts/fls_theme/home.blade.php
@@ -3,8 +3,7 @@
 
 @section('content')
 
-<div class="container-fluid intro p-0"
-  style="height: 100vh; background: url('https://www.aerofarallones.com/image/new/Home.png')">
+<div class="container-fluid intro p-0" style="">
   <div class="row-home w-100 h-100 m-0" style="background: rgba(0, 10, 82, 0.5)">
     {{--- INICIO TRABAJO --}}
   </div>

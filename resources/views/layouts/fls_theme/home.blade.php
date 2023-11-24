ddae43d6b0ee19e39baa03fc6557a0580e999fd7
rcomunica <julir2772@gmail.com>
2023-11-24 15:08:14 -0500

    Staff Cards ~ Staff panel started (80% responsive)

--- resources/views/layouts/fls_theme/home.blade.php
+++ resources/views/layouts/fls_theme/home.blade.php
@@ -21,6 +21,87 @@
     <div class="col"></div>
     <div class="col"></div>
   </div>
+  <div class="container-fluid d-grid w-100 p-5 h-100 align-items-center gap-5">
+    <div class="container">
+      <h2 class="staff__title">STAFF</h2>
+    </div>
+    <div class="container text-center h-100 px-5 d-flex justify-content-center">
+      <div class="staffCard__container h-100 w-100">
+        <div class="staffCard__item">
+          <div class="staffCard__image">
+            <img class="rounded-pill" src="{{asset('fls-theme/frontend/img/marlon.jpg')}}" alt="staff_photo">
+          </div>
+          <div class="staffCard">
+            <div class="staffCard__name"><span>Julian Ramirez</span></div>
+            <div class="staffCard__position"><span>Webmaster</span></div>
+            <div class="staffCard__ivaoCard">
+              <img src="https://status.ivao.aero/653841.png" alt="">
+            </div>
+          </div>
+        </div>
+        <div class="staffCard__item">
+          <div class="staffCard__image">
+            <img class="rounded-pill" src="{{asset('fls-theme/frontend/img/marlon.jpg')}}" alt="staff_photo">
+          </div>
+          <div class="staffCard">
+            <div class="staffCard__name"><span>Julian Ramirez</span></div>
+            <div class="staffCard__position"><span>Webmaster</span></div>
+            <div class="staffCard__ivaoCard">
+              <img src="https://status.ivao.aero/653841.png" alt="">
+            </div>
+          </div>
+        </div>
+        <div class="staffCard__item">
+          <div class="staffCard__image">
+            <img class="rounded-pill" src="{{asset('fls-theme/frontend/img/marlon.jpg')}}" alt="staff_photo">
+          </div>
+          <div class="staffCard">
+            <div class="staffCard__name"><span>Julian Ramirez</span></div>
+            <div class="staffCard__position"><span>Webmaster</span></div>
+            <div class="staffCard__ivaoCard">
+              <img src="https://status.ivao.aero/653841.png" alt="">
+            </div>
+          </div>
+        </div>
+        <div class="staffCard__item">
+          <div class="staffCard__image">
+            <img class="rounded-pill" src="{{asset('fls-theme/frontend/img/marlon.jpg')}}" alt="staff_photo">
+          </div>
+          <div class="staffCard">
+            <div class="staffCard__name"><span>Julian Ramirez</span></div>
+            <div class="staffCard__position"><span>Webmaster</span></div>
+            <div class="staffCard__ivaoCard">
+              <img src="https://status.ivao.aero/653841.png" alt="">
+            </div>
+          </div>
+        </div>
+        <div class="staffCard__item">
+          <div class="staffCard__image">
+            <img class="rounded-pill" src="{{asset('fls-theme/frontend/img/marlon.jpg')}}" alt="staff_photo">
+          </div>
+          <div class="staffCard">
+            <div class="staffCard__name"><span>Julian Ramirez</span></div>
+            <div class="staffCard__position"><span>Webmaster</span></div>
+            <div class="staffCard__ivaoCard">
+              <img src="https://status.ivao.aero/653841.png" alt="">
+            </div>
+          </div>
+        </div>
+        <div class="staffCard__item">
+          <div class="staffCard__image">
+            <img class="rounded-pill" src="{{asset('fls-theme/frontend/img/marlon.jpg')}}" alt="staff_photo">
+          </div>
+          <div class="staffCard">
+            <div class="staffCard__name"><span>Julian Ramirez</span></div>
+            <div class="staffCard__position"><span>Webmaster</span></div>
+            <div class="staffCard__ivaoCard">
+              <img src="https://status.ivao.aero/653841.png" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
 
 @endsection

6d6fc2800076a304fa77d3d96d79d7410edf3e10
rcomunica <julir2772@gmail.com>
2023-12-03 20:25:15 -0500

    Navbard updated and started by k-pisito

--- resources/views/layouts/fls_theme/home_nav.blade.php
+++ resources/views/layouts/fls_theme/home_nav.blade.php
@@ -1,18 +1,17 @@
-<nav class="fixed-top d-flex px-3 pt-5" id="nav-home" style="background: transparent" data-color="#000a54">
-  <div class="container-fluid d-flex justify-content-between">
-    <a class="navbar-brand" href="{{route('frontend.home')}}"><img
-        src="https://www.aerofarallones.com/image/new/Logo_FLS_CO_TXT_S.png" width="250px" alt=""></a>
-    <button class="fs-4 bg-transparent rounded" style="border: none" type="button" data-bs-toggle="offcanvas"
-      data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
-      <i class="fa fa-bars text-white" aria-hidden="true"></i>
+<nav class="navbar fixed-top px-3 pt-5" id="nav-home" style="background: transparent" data-color="#000a54">
+  <div class=" container-fluid">
+    <a class="navbar-brand" href="#"><img width="250px"
+        src="https://www.aerofarallones.com/image/new/Logo_FLS_CO_TXT_S.png" alt=""></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
+      aria-controls="offcanvasNavbar">
+      <span class="navbar-toggler-icon"></span>
     </button>
-    {{--Navigation menu (Don't touch)--}}
-    <div class="offcanvas offcanvas-end" style="background: #FFFFFF" tabindex="-1" id="offcanvasDarkNavbar"
-      aria-labelledby="offcanvasDarkNavbarLabel">
+    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
       <div class="offcanvas-header">
-        <h5 class="offcanvas-title fs-3 font-montbold" style="color: #000a54" id="offcanvasDarkNavbarLabel">
-          AeroFarallones</h5>
-        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        <h5 class="offcanvas-title fs-3 font-montbold" id="offcanvasNavbarLabel" style="color: #000a54">
+          AeroFarallones
+        </h5>
+        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
       </div>
       @include('layouts.fls_theme.nav_links')
     </div>
--- resources/views/layouts/fls_theme/nav.blade.php
+++ resources/views/layouts/fls_theme/nav.blade.php
@@ -1,25 +1,19 @@
-<nav class="navbar" style="background: #000a54">
+<nav class="navbar bg-light fixed-top">
   <div class="container-fluid">
-    <a class="navbar-brand" href="{{route('frontend.home')}}"><img
-        src="https://www.aerofarallones.com/image/new/Logo_FLS_CO_TXT_S.png" width="250px" alt=""></a>
-    <button class="fs-4 bg-transparent rounded" style="border: none" type="button" data-bs-toggle="offcanvas"
-      data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
-      <i class="fa fa-bars text-white" aria-hidden="true"></i>
+    <a class="navbar-brand" href="#">Offcanvas navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
+      aria-controls="offcanvasNavbar">
+      <span class="navbar-toggler-icon"></span>
     </button>
-
-    {{--Navigation menu (Don't touch)--}}
-    <div class="offcanvas offcanvas-end" style="background: #000a54" tabindex="-1" id="offcanvasDarkNavbar"
-      aria-labelledby="offcanvasDarkNavbarLabel">
+    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
       <div class="offcanvas-header">
-        <h5 class="offcanvas-title text-white fs-3" id="offcanvasDarkNavbarLabel">
-          AeroFarallones</h5>
-        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
       </div>
       <div class="offcanvas-body">
         <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page"
-              href="{{route('frontend.dashboard.index')}}">{{__('common.dashboard')}}</a>
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">Link</a>
@@ -28,7 +22,7 @@
             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               Dropdown
             </a>
-            <ul class="dropdown-menu dropdown-menu-dark">
+            <ul class="dropdown-menu">
               <li><a class="dropdown-item" href="#">Action</a></li>
               <li><a class="dropdown-item" href="#">Another action</a></li>
               <li>
@@ -38,6 +32,10 @@
             </ul>
           </li>
         </ul>
+        <form class="d-flex mt-3" role="search">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
       </div>
     </div>
   </div>

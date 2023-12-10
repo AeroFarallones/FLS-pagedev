f583735476577b95e680da96883e22c0f02c4899
martsime99 <martsime99@gmail.com>
2023-12-10 13:00:42 -0500

    Added map to pireps page

--- resources/views/layouts/fls_theme/pireps/show.blade.php
+++ resources/views/layouts/fls_theme/pireps/show.blade.php
@@ -1,66 +1,64 @@
 @extends('app')
 @include('theme_helpers')
+@php
+  $AuthCheck = Auth::check();
+  $units = isset($units) ? $units : DT_GetUnits();
+@endphp
 @section('title', trans_choice('common.pirep', 1).' '.$pirep->ident)
 
 @section('content')
-<div class="row">
+<div class="row pt-3">
   <div class="col-sm-8">
-    <div class="card mb-2">
-      <div class="card-header p-1">
+    <div class="card mb-2 dashboard-card">
+      <div class="card-header p-1 dashboard-card-body">
         <h5 class="m-1">
           @if(filled($pirep->flight_id) && !str_contains($pirep->route_code, 'PF'))
-            <a href="{{ route('frontend.flights.show', [$pirep->flight_id]) }}"><i class="fas fa-paper-plane mx-1" title="Flight Details"></i></a>
+          <a href="{{ route('frontend.flights.show', [$pirep->flight_id]) }}"><i class="fas fa-paper-plane mx-1"
+              title="Flight Details"></i></a>
           @endif
           {{ optional($pirep->airline)->code.' '.$pirep->flight_number }}
           {{ ' | '.optional($pirep->dpt_airport)->location.' > '.optional($pirep->arr_airport)->location }}
-          <i class="fas fa-file-upload float-end"></i>
         </h5>
       </div>
       <div class="card-body p-1">
         <div class="row row-cols-md-3">
           <div class="col-md-5 text-start">
-              <i class="fas fa-plane-departure m-1"></i>
-              <a href="{{ route('frontend.airports.show', [$pirep->dpt_airport_id]) }}">
-                {{ optional($pirep->dpt_airport)->full_name ?? $pirep->dpt_airport_id }}
-              </a>
+            <i class="fas fa-plane-departure m-1 color-fls"></i>
+            <a href="{{ route('frontend.airports.show', [$pirep->dpt_airport_id]) }}">
+              {{ optional($pirep->dpt_airport)->full_name ?? $pirep->dpt_airport_id }}
+            </a>
           </div>
           <div class="col-md-2 text-center">
             @if(!empty($pirep->distance))
-    <div class="row">
-      <div class="col-12">
-        <div class="progress" style="margin: 20px 0;">
-          <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40" aria-valuemin="0"
-            aria-valuemax="100" style="width: {{$pirep->progress_percent}}%;">
-          </div>
-        </div>
-      </div>
-    </div>
-    @endif
+            <div class="row">
+              {{ Fls_ConvertDistance($pirep->distance) }}
+            </div>
+            @endif
           </div>
           <div class="col-md-5 text-end">
             <a href="{{ route('frontend.airports.show', [$pirep->arr_airport_id]) }}">
               {{ optional($pirep->arr_airport)->full_name ?? $pirep->arr_airport_id }}
             </a>
-            <i class="fas fa-plane-arrival m-1"></i>
+            <i class="fas fa-plane-arrival m-1 color-fls"></i>
           </div>
         </div>
         <div class="row row-cols-md-3">
           <div class="col-md text-start">
             @if(filled($pirep->block_off_time))
-              <i class="fas fa-clock float-start m-1" title="Off Block"></i>
-              {{ $pirep->block_off_time->format('H:i | l d.M.Y') }}
+            <i class="fas fa-clock float-start m-1 color-fls" title="Off Block"></i>
+            {{ $pirep->block_off_time->format('H:i | l d.M.Y') }}
             @endif
           </div>
           <div class="col-md text-center">
             @if(filled($pirep->flight_time))
-              <i class="fas fa-stopwatch m-1" title="Block Time"></i>
-              {{ DT_ConvertMinutes($pirep->flight_time, '%2dh %2dm') }}
+            <i class="fas fa-stopwatch m-1 color-fls" title="Block Time"></i>
+            {{ DT_ConvertMinutes($pirep->flight_time, '%2dh %2dm') }}
             @endif
           </div>
           <div class="col-md text-end">
             @if($pirep->block_on_time > $pirep->block_off_time)
-              <i class="fas fa-clock float-end m-1" title="On Block"></i>
-              {{ $pirep->block_on_time->format('H:i | l d.M.Y') }}
+            <i class="fas fa-clock float-end m-1 color-fls" title="On Block"></i>
+            {{ $pirep->block_on_time->format('H:i | l d.M.Y') }}
             @endif
           </div>
         </div>
@@ -68,7 +66,7 @@
       <div class="card-footer bg-transparent p-1">
         <div class="progress" style="height: 20px;">
           <div
-            class="progress-bar bg-success @if(blank($pirep->block_on_time)) progress-bar-striped progress-bar-animated @endif text-black"
+            class="progress-bar progress-bar-fls @if(blank($pirep->block_on_time)) progress-bar-striped progress-bar-animated @endif"
             role="progressbar" style="width: {{ $pirep->progress_percent }}%;"
             aria-valuenow="{{ $pirep->progress_percent }}" aria-valuemin="0" aria-valuemax="100">
             {{ $pirep->progress_percent }}%
@@ -83,216 +81,137 @@
           <div class="col text-end">
             {!! DT_RouteCode($pirep->route_code, 'button') !!} {!! DT_RouteLeg($pirep->route_leg, 'button') !!}
             @if(!$pirep->read_only && !$pirep->source == 1 && $user && $pirep->user_id === $user->id)
-              <form method="get" action="{{ route('frontend.pireps.edit', $pirep->id) }}">
-                @csrf
-                <button class="btn btn-sm btn-info m-0 mx-1 p-0 px-1">@lang('common.edit')</button>
-              </form>
-              <form method="post" action="{{ route('frontend.pireps.submit', $pirep->id) }}">
-                @csrf
-                <button class="btn btn-sm btn-success m-0 mx-1 p-0 px-1">@lang('common.submit')</button>
-              </form>
+            <form method="get" action="{{ route('frontend.pireps.edit', $pirep->id) }}">
+              @csrf
+              <button class="btn btn-sm btn-info m-0 mx-1 p-0 px-1">@lang('common.edit')</button>
+            </form>
+            <form method="post" action="{{ route('frontend.pireps.submit', $pirep->id) }}">
+              @csrf
+              <button class="btn btn-sm btn-success m-0 mx-1 p-0 px-1">@lang('common.submit')</button>
+            </form>
             @endif
           </div>
         </div>
       </div>
     </div>
-    <h2>{{ $pirep->ident }} : {{ $pirep->dpt_airport_id }} to {{ $pirep->arr_airport_id }}</h2>gi
+    <div class="col-md-12">
+      @include('pireps.map')
+    </div>
   </div>
 
   <div class="col-sm-4">
-    {{-- Show the link to edit if it can be edited --}}
-    @if (!empty($pirep->simbrief))
-    <a href="{{ url(route('frontend.simbrief.briefing', [$pirep->simbrief->id])) }}" class="btn btn-outline-info">View
-      SimBrief</a>
-    @endif
-
-    @if(!$pirep->read_only && $user && $pirep->user_id === $user->id)
-    <div class="float-right" style="margin-bottom: 10px;">
-      <form method="get" action="{{ route('frontend.pireps.edit', $pirep->id) }}" style="display: inline">
-        @csrf
-        <button class="btn btn-outline-info">@lang('common.edit')</button>
-      </form>
-      &nbsp;
-      <form method="post" action="{{ route('frontend.pireps.submit', $pirep->id) }}" style="display: inline">
-        @csrf
-        <button class="btn btn-outline-success">@lang('common.submit')</button>
-      </form>
-    </div>
-    @endif
-  </div>
-</div>
-
-<div class="row">
-  <div class="col-8">
-    <div class="row">
-      {{--
-      DEPARTURE INFO
-      --}}
-      <div class="col-6 text-left">
-        <h4>
-          {{$pirep->dpt_airport->location}}
-        </h4>
-        <p>
-          <a href="{{route('frontend.airports.show', $pirep->dpt_airport_id)}}">
-            {{ $pirep->dpt_airport->full_name }} ({{ $pirep->dpt_airport_id }})</a>
-          <br />
-          @if($pirep->block_off_time)
-          {{ $pirep->block_off_time->toDayDateTimeString() }}
-          @endif
-        </p>
-      </div>
-
-      {{--
-      ARRIVAL INFO
-      --}}
-      <div class="col-6 text-right">
-        <h4>
-          {{$pirep->arr_airport->location}}
-        </h4>
-        <p>
-          <a href="{{route('frontend.airports.show', $pirep->arr_airport_id)}}">
-            {{ $pirep->arr_airport->full_name }} ({{ $pirep->arr_airport_id }})</a>
-          <br />
-          @if($pirep->block_on_time)
-          {{ $pirep->block_on_time->toDayDateTimeString() }}
-          @endif
-        </p>
-      </div>
-    </div>
-
-    @if(!empty($pirep->distance))
-    <div class="row">
-      <div class="col-12">
-        <div class="progress" style="margin: 20px 0;">
-          <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40" aria-valuemin="0"
-            aria-valuemax="100" style="width: {{$pirep->progress_percent}}%;">
-          </div>
-        </div>
-      </div>
-    </div>
-    @endif
-
-    <div class="row">
-      <div class="col-12">
-        @include('pireps.map')
-      </div>
-    </div>
-  </div>
-
-  {{--
+    {{--
 
   RIGHT SIDEBAR
 
   --}}
+  <table class="table table-striped">
+    <tr>
+      <td width="30%">@lang('common.state')</td>
+      <td>
+        <div class="badge badge-info">
+          {{ PirepState::label($pirep->state) }}
+        </div>
+      </td>
+    </tr>
 
-  <div class="col-4">
-    <table class="table table-striped">
-      <tr>
-        <td width="30%">@lang('common.state')</td>
-        <td>
-          <div class="badge badge-info">
-            {{ PirepState::label($pirep->state) }}
-          </div>
-        </td>
-      </tr>
-
-      @if ($pirep->state !== PirepState::DRAFT)
-      <tr>
-        <td width="30%">@lang('common.status')</td>
-        <td>
-          <div class="badge badge-info">
-            {{ PirepStatus::label($pirep->status) }}
-          </div>
-        </td>
-      </tr>
-      @endif
-
-      <tr>
-        <td>@lang('pireps.source')</td>
-        <td>{{ PirepSource::label($pirep->source) }}</td>
-      </tr>
-
-      <tr>
-        <td>@lang('flights.flighttype')</td>
-        <td>{{ \App\Models\Enums\FlightType::label($pirep->flight_type) }}</td>
-      </tr>
-
-      <tr>
-        <td>@lang('pireps.filedroute')</td>
-        <td>{{ $pirep->route }}</td>
-      </tr>
-
-      <tr>
-        <td>{{ trans_choice('common.note', 2) }}</td>
-        <td>{{ $pirep->notes }}</td>
-      </tr>
-
-      @if($pirep->score && $pirep->landing_rate)
-      <tr>
-        <td>Score</td>
-        <td>{{ $pirep->score }}</td>
-      </tr>
-      <tr>
-        <td>Landing Rate</td>
-        <td>{{ number_format($pirep->landing_rate) }}</td>
-      </tr>
-      @endif
-
-      <tr>
-        <td>@lang('pireps.filedon')</td>
-        <td>{{ show_datetime($pirep->created_at) }}</td>
-      </tr>
-
-    </table>
-
-    @if(count($pirep->fields) > 0)
-    <div class="separator"></div>
+    @if ($pirep->state !== PirepState::DRAFT)
+    <tr>
+      <td width="30%">@lang('common.status')</td>
+      <td>
+        <div class="badge badge-info">
+          {{ PirepStatus::label($pirep->status) }}
+        </div>
+      </td>
+    </tr>
     @endif
 
-    @if(count($pirep->fields) > 0)
-    <h5>{{ trans_choice('common.field', 2) }}</h5>
-    <table class="table table-hover table-condensed">
-      <thead>
-        <th>@lang('common.name')</th>
-        <th>{{ trans_choice('common.value', 1) }}</th>
-      </thead>
-      <tbody>
-        @foreach($pirep->fields as $field)
-        <tr>
-          <td>{{ $field->name }}</td>
-          <td>{{ $field->value }}</td>
-        </tr>
-        @endforeach
-      </tbody>
-    </table>
+    <tr>
+      <td>@lang('pireps.source')</td>
+      <td>{{ PirepSource::label($pirep->source) }}</td>
+    </tr>
+
+    <tr>
+      <td>@lang('flights.flighttype')</td>
+      <td>{{ \App\Models\Enums\FlightType::label($pirep->flight_type) }}</td>
+    </tr>
+
+    <tr>
+      <td>@lang('pireps.filedroute')</td>
+      <td>{{ $pirep->route }}</td>
+    </tr>
+
+    <tr>
+      <td>{{ trans_choice('common.note', 2) }}</td>
+      <td>{{ $pirep->notes }}</td>
+    </tr>
+
+    @if($pirep->score && $pirep->landing_rate)
+    <tr>
+      <td>Score</td>
+      <td>{{ $pirep->score }}</td>
+    </tr>
+    <tr>
+      <td>Landing Rate</td>
+      <td>{{ number_format($pirep->landing_rate) }}</td>
+    </tr>
     @endif
 
-    {{--
+    <tr>
+      <td>@lang('pireps.filedon')</td>
+      <td>{{ show_datetime($pirep->created_at) }}</td>
+    </tr>
+
+  </table>
+
+  @if(count($pirep->fields) > 0)
+  <div class="separator"></div>
+  @endif
+
+  @if(count($pirep->fields) > 0)
+  <h5>{{ trans_choice('common.field', 2) }}</h5>
+  <table class="table table-hover table-condensed">
+    <thead>
+      <th>@lang('common.name')</th>
+      <th>{{ trans_choice('common.value', 1) }}</th>
+    </thead>
+    <tbody>
+      @foreach($pirep->fields as $field)
+      <tr>
+        <td>{{ $field->name }}</td>
+        <td>{{ $field->value }}</td>
+      </tr>
+      @endforeach
+    </tbody>
+  </table>
+  @endif
+
+  {{--
     Show the fares that have been entered
     --}}
-    @if(count($pirep->fares) > 0)
-    <div class="separator"></div>
-    <div class="row">
-      <div class="col-12">
-        <h5>{{ trans_choice('pireps.fare', 2) }}</h5>
-        <table class="table table-hover table-condensed">
-          <thead>
-            <th>@lang('pireps.class')</th>
-            <th>@lang('pireps.count')</th>
-          </thead>
-          <tbody>
-            @foreach($pirep->fares as $fare)
-            <tr>
-              <td>{{ $fare->name }} ({{ $fare->code }})</td>
-              <td>{{ $fare->count }}</td>
-            </tr>
-            @endforeach
-          </tbody>
-        </table>
-      </div>
+  @if(count($pirep->fares) > 0)
+  <div class="separator"></div>
+  <div class="row">
+    <div class="col-12">
+      <h5>{{ trans_choice('pireps.fare', 2) }}</h5>
+      <table class="table table-hover table-condensed">
+        <thead>
+          <th>@lang('pireps.class')</th>
+          <th>@lang('pireps.count')</th>
+        </thead>
+        <tbody>
+          @foreach($pirep->fares as $fare)
+          <tr>
+            <td>{{ $fare->name }} ({{ $fare->code }})</td>
+            <td>{{ $fare->count }}</td>
+          </tr>
+          @endforeach
+        </tbody>
+      </table>
     </div>
-    @endif
   </div>
+  @endif
+</div>
 </div>
 
 @if(count($pirep->acars_logs) > 0)
@@ -332,5 +251,29 @@
     </div>
   </div>
 </div>
+    {{-- Show the link to edit if it can be edited --}}
+    @if (!empty($pirep->simbrief))
+    <a href="{{ url(route('frontend.simbrief.briefing', [$pirep->simbrief->id])) }}" class="btn btn-outline-info">View
+      SimBrief</a>
+    @endif
+
+    @if(!$pirep->read_only && $user && $pirep->user_id === $user->id)
+    <div class="float-right" style="margin-bottom: 10px;">
+      <form method="get" action="{{ route('frontend.pireps.edit', $pirep->id) }}" style="display: inline">
+        @csrf
+        <button class="btn btn-outline-info">@lang('common.edit')</button>
+      </form>
+      &nbsp;
+      <form method="post" action="{{ route('frontend.pireps.submit', $pirep->id) }}" style="display: inline">
+        @csrf
+        <button class="btn btn-outline-success">@lang('common.submit')</button>
+      </form>
+    </div>
+    @endif
+  </div>
+ 
+</div>    
+
+
 @endif
 @endsection

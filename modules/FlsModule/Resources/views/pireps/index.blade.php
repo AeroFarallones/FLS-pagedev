2a44332e6a7926490b9ad3b953e3fdb50bfb7858
martsime99 <martsime99@gmail.com>
2023-12-12 22:40:48 -0500

    Pilot reports completed

--- modules/FlsModule/Resources/views/pireps/index.blade.php
+++ modules/FlsModule/Resources/views/pireps/index.blade.php
@@ -5,19 +5,18 @@
 @if(!$pireps->count())
 <div class="alert alert-info mb-1 p-1 px-2 fw-bold">No Pilot Reports!</div>
 @else
-<div class="row">
-  <div class="col">
+<div class="row pt-3">
+  <div class="col-md-12">
     <div class="card mb-2">
-      <div class="card-header p-1">
-        <h5 class="m-1">
+      <div class="card-header p-1 dashboard-card-body">
+        <h3 class="m-1">
           @lang('FlsModule::common.reports')
-          <i class="fas fa-upload float-end"></i>
-        </h5>
+        </h3>
       </div>
       <div class="card-body p-0 overflow-auto table-responsive" style="max-height: 78vh;">
         @include('FlsModule::pireps.table')
       </div>
-      <div class="card-footer p-0 px-1 text-end fw-bold small">
+      <div class="card-footer p-0 px-3 text-end fw-bold small card-footer-fls">
         @lang('FlsModule::common.paginate', ['first' => $pireps->firstItem(), 'last' => $pireps->lastItem(), 'total' =>
         $pireps->total()])
       </div>
--- resources/views/layouts/fls_theme/pireps/edit.blade.php
+++ resources/views/layouts/fls_theme/pireps/edit.blade.php
@@ -1,19 +1,23 @@
 @extends('app')
 @section('title', __('pireps.editflightreport'))
 @section('content')
-  <div class="row">
-    <div class="col-md-12">
-      <h2>@lang('pireps.editflightreport')</h2>
-      @include('flash::message')
-      {{ Form::model($pirep, [
-              'route' => ['frontend.pireps.update', $pirep->id],
-              'class' => 'form-group',
-              'method' => 'patch']) }}
-
-      @include('pireps.fields')
-
-      {{ Form::close() }}
+<div class="row pt-3">
+  <div class="col-md-12">
+    <div class="card mb-2">
+      <div class="card-header p-1 dashboard-card-body">
+        <h3 class="m1">@lang('pireps.editflightreport')</h3>
+      </div>
+      <div class="card-body p-1">
+        @include('flash::message')
+        {{ Form::model($pirep, [
+                  'route' => ['frontend.pireps.update', $pirep->id],
+                  'class' => 'form-group',
+                  'method' => 'patch']) }}
+        @include('pireps.fields')
+        {{ Form::close() }}
+      </div>
     </div>
   </div>
+</div>
 @endsection
 @include('pireps.scripts')

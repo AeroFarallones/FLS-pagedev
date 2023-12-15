4dd95a76b43915322d230a2d493b8bbdfa674160
Hydroxya22 <mjgarcia0110j@gmail.com>
2023-12-14 20:14:31 -0500

    News stylished

--- modules/FlsModule/Resources/views/news/index.blade.php
+++ modules/FlsModule/Resources/views/news/index.blade.php
@@ -5,12 +5,12 @@
 @if(!$allnews->count())
 <div class="alert alert-info mb-1 p-1 px-2 fw-bold">No News!</div>
 @else
-<div class="row row-cols-lg-2">
+<div class="row row-cols-lg-2 pt-3">
   @foreach($allnews as $news)
   <div class="col-lg">
     <div class="card mb-2">
-      <div class="card-header p-1">
-        <h5 class="m-1">
+      <div class="card-header p-1 dashboard-card-body">
+        <h5 class="m-1 text-white">
           {{ $news->subject }}
           <i class="fas fa-book-open float-end"></i>
         </h5>
@@ -18,7 +18,7 @@
       <div class="card-body p-1 text-start">
         {!! $news->body !!}
       </div>
-      <div class="card-footer p-0 px-1 text-end small fw-bold">
+      <div class="card-footer p-0 px-1 text-end small fw-bold card-footer-fls">
         <span class="float-start">{{ optional($news->user)->name_private }}</span>
         {{ $news->created_at->format('d.M.Y H:i') }}
       </div>
